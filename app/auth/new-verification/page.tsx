d88c9a61cf4a30e4f72105727f0f1d59f89555ab
zhouxiao <zhouxiao@npn.sg>
2024-01-09 16:58:08 +0800

    feat: Email verification

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,8 @@
+import { NewVerificationForm } from '@/components/auth/new-verification-form'
+import type { NextPage } from 'next'
+
+const NewVerificationPage: NextPage = () => {
+  return <NewVerificationForm />
+}
+
+export default NewVerificationPage
