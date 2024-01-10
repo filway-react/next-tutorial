1334b24ee98c8ed645ea1fe1b2de7d9b13b6fefb
zhouxiao <zhouxiao@npn.sg>
2024-01-10 09:57:04 +0800

    feat: Reset password form

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,8 @@
+import { NewPasswordForm } from '@/components/auth/new-password-form'
+import type { NextPage } from 'next'
+
+const NewPasswordPage: NextPage = () => {
+  return <NewPasswordForm />
+}
+
+export default NewPasswordPage
